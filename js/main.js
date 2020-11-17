00bdbc6c03d0a0dc4701169f192e52ee0a242d47
GiDLdev <contact.gildas@free.fr>
2020-11-17 17:25:05 +0100

    mardi17nov

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let range = document.getElementsByClassName('range'); 
+let valeur = document.getAttribute('step');
+//let newvalue = value + 10;
+
+function changeAtt(){
+  range.toggleAttribute('valeur' [20]);
+
+}
